c5843f99c057ac3f1387a53a844fa2ded3584c07
Michal Hotovec <michal.hotovec@gmail.com>
2019-02-17 22:34:27 +0100

    template + static files

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello from simpleDjango:App:index updated")
+    template = loader.get_template('index.html')
+    context = {
+        'title': 'app title',
+    }
+    return HttpResponse(template.render(context, request))
